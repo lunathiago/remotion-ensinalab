9dcdbc4a5827f5f5815bd17ca466c168595c2990
lunathiago <lunathiago14@gmail.com>
2025-11-16 03:01:07 +0000

    Fixing deploy

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,15 +1,10 @@
-// FORMATO NOVO - CORRETO
+// remotion.config.ts
 import {defineConfig} from 'remotion';
 
 export default defineConfig({
   // O nome do seu bucket S3 será gerado a partir disso
-  // ex: remotion-render-my-video-xxxx
-  s3Bucket: 'remotion-render-ensinalab', // <- Troque para um nome único se quiser
+  s3Bucket: 'remotion-render-ensinalab',
   
   // A região da AWS para fazer o deploy.
-  // Precisa ser a mesma que você usou no bootstrap!
   lambdaRegion: 'us-east-1',
-
-  // Opcional: outras configurações que você pode querer
-  // ...
 });
