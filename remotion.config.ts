820f31ad9648003f36b3fa0753a6ff112a28bf81
lunathiago <lunathiago14@gmail.com>
2025-11-16 03:11:06 +0000

    Fixing Deploy Location

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,10 +1,6 @@
-// remotion.config.ts
-import {defineConfig} from 'remotion';
+const {defineConfig} = require('remotion');
 
-export default defineConfig({
-  // O nome do seu bucket S3 será gerado a partir disso
+module.exports = defineConfig({
   s3Bucket: 'remotion-render-ensinalab',
-  
-  // A região da AWS para fazer o deploy.
   lambdaRegion: 'us-east-1',
 });
